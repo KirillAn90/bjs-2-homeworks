95074e0e170bd75c7480233286bf4a7f21979678
AnKS <kiril-an@mail.ru>
2025-01-18 13:12:25 +0300

    solutionTask

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,19 +1,20 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  let x1;
-  let x2;
-  let d = b ** 2 - 4 * a * c;
-  
-  if (d > 0) {
-    x1 = (-b + Math.sqrt(d)) / (2 * a);
-    x2 = (-b - Math.sqrt(d)) / (2 * a);
-    arr.push(x1, x2);
-  } else if (d === 0) {
-    x1 = -b / (2 * a);
-    arr.push(x1);
-  }
-  return arr;
+	let arr = [];
+	let x1;
+	let x2;
+	let d = b ** 2 - 4 * a * c;
+
+	if (d > 0) {
+		x1 = (-b + Math.sqrt(d)) / (2 * a);
+		x2 = (-b - Math.sqrt(d)) / (2 * a);
+		arr.push(x1, x2);
+	} else if (d === 0) {
+		x1 = -b / (2 * a);
+		arr.push(x1);
+	}
+	return arr;
 }
 
 console.log(solveEquation());
