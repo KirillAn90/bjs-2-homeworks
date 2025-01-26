1977a41392c0a541140aedd4bd2fd1132ffbdd56
AnKS <kiril-an@mail.ru>
2025-01-26 17:47:11 +0300

    changesFirstTasks

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,6 +1,16 @@
 function getArrayParams(...arr) {
-
-  return { min: min, max: max, avg: avg };
+	let sum = 0;
+	let min = Infinity;
+	let max = -Infinity;
+	for (i = 0; i < arr.length; i++) {
+		sum += arr[i];
+	}
+	let avg = sum / arr.length;
+	return {
+		min: min,
+		max: max,
+		avg: Number(avg.toFixed(1))
+	};
 }
 
 function summElementsWorker(...arr) {
